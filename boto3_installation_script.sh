f8dd8510e35393923e1a49f530d730b0feb38f82
qubetechnology <50607022+qubetechnology@users.noreply.github.com>
2019-05-15 11:58:02 +0530

    Create boto3_installation_script.sh

---
+++ boto3_installation_script.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+
+
+###############################################################
+#Purpose      :  BOTO3 Installation
+#Version      :  0.1
+#Author       :  Anand Reddy
+#Created      :  10/01/19
+###############################################################
+
+sudo yum install python python-dev python-pip -y
+
+sudo yum install boto3 -y
